7d84d6c309a84bed17a393ecbd9aa14830db9a0c
Corina Talavera <corina.talavera@gmail.com>
2018-11-10 19:20:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,16 @@
-class Dog 
+class Dog
   def initialize(breed)
     @breed = breed
   end
-  def breed
+ 
+  def breed=(breed)
     @breed = breed
   end
+ 
   def breed
     @breed
   end
+end
   
 laika = Dog.new("mutt")
 laika.breed #=> "mutt"
